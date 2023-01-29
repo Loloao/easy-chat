2d7bf9b9c7ef6a2e30db0dbac5969b6c8dd35e8b
Loloao <1312213036@qq.com>
2023-01-30 02:53:35 +0800

    :heavy_plus_sign: add .env

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,26 @@
 import express from "express";
+import { chats } from "./data/data";
+import dotEnv from "dotenv";
+
+// 加载 .env 文件
+dotEnv.config();
 
 const app = express();
-app.listen(9000, () => console.log("Server is listen at port 5000"));
-// const express = require("express");
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.get("/api/chats", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chats/:id", (req, res) => {
+  const chat = chats.find((v) => {
+    v._id === req.params.id;
+  });
+  res.send(chat);
+});
+
+const port = process.env.PORT || 8001;
+app.listen(port, () => console.log("Server is listen at port " + port));
