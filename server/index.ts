d00df005636f8dec090f51b2502ec9102c882ea7
Loloao <l1312213036@outlook.com>
2023-02-18 21:57:56 +0800

    :tada: complete project

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import dotEnv from "dotenv";
-import connectDB from "./config/db";
 import colors from "colors";
+import { Server } from "socket.io";
+import path from "path";
+
 import userRouters from "./routes/userRoutes";
 import chatRouters from "./routes/chatRoutes";
 import messageRouters from "./routes/messageRoutes";
+import connectDB from "./config/db";
 import { notFound, errorHandler } from "./middleware/errorMiddleware";
-import { Server } from "socket.io";
-import { SOCKET_EVENT } from "@shared/enums";
+import { SOCKET_EVENT } from "../shared/enums";
+import { Message, User } from "../shared/types";
 
 // 加载 .env 文件
 dotEnv.config();
@@ -21,6 +24,20 @@ app.use(express.json());
 app.use("/api/user", userRouters);
 app.use("/api/chat", chatRouters);
 app.use("/api/message", messageRouters);
+
+// ------------- Deploymeng ------------------
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "client", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is Running Successfully");
+  });
+}
+// ------------- Deploymeng ------------------
 
 app.use(notFound);
 app.use(errorHandler);
@@ -36,15 +53,40 @@ const io = new Server(server, {
     origin: process.env.CLIENT_ADDRESS,
   },
 });
+const {
+  CONNECTED,
+  SET_UP,
+  MESSAGE_RECEIVED,
+  CONNECTION,
+  JOIN_CHAT,
+  NEW_MESSAGE,
+} = SOCKET_EVENT;
 
-io.on(SOCKET_EVENT.CONNECTION, (socket) => {
-  socket.on(SOCKET_EVENT.SET_UP, (userData) => {
+io.on(CONNECTION, (socket) => {
+  console.log("connected to socket.io");
+  socket.on(SET_UP, (userData) => {
     socket.join(userData._id);
-    socket.emit(SOCKET_EVENT.CONNECTED);
+    socket.emit(CONNECTED);
   });
 
-  socket.on(SOCKET_EVENT.JOIN_CHAT, (room) => {
+  socket.on(JOIN_CHAT, (room) => {
     socket.join(room);
     console.log("User Joined Room: " + room);
+  });
+
+  socket.on(NEW_MESSAGE, (newMessageRecieved) => {
+    const chat = newMessageRecieved.chat;
+
+    if (!chat.users) return console.log("users not found");
+
+    chat.users.forEach((v: User) => {
+      if (v._id === newMessageRecieved.sender._id) return;
+
+      socket.in(v._id).emit(MESSAGE_RECEIVED, newMessageRecieved);
+    });
+  });
+
+  socket.off(SET_UP, (userData) => {
+    socket.leave(userData._id);
   });
 });
